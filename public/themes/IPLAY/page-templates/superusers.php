e4e009add08b3ac2772a33a5aa2597ec714366c2
cjcmattsson <cjcmattsson@gmail.com>
2018-04-04 13:49:57 +0200

    Sort of completed SUPERUSERS section

--- public/themes/IPLAY/page-templates/superusers.php
+++ public/themes/IPLAY/page-templates/superusers.php
@@ -21,11 +21,14 @@ $superusers = collect(get_posts($args))->shuffle()->take(3);
 
 
         <div class="header-super">
+            <p>Our</p>
             <h1 class=""><?php echo $fields['hero_text'] ?></h1>
         </div>
 
         <div class="content-super">
             <div class="superusers">
+              <div class="super-text-and-cta">
+
                 <div class="athletes-info-text">
                     <?php foreach($superusers as $user): ?>
                         <?php $post_id = $user->ID; ?>
@@ -39,6 +42,13 @@ $superusers = collect(get_posts($args))->shuffle()->take(3);
                         <?php endif; ?>
                     <?php endforeach; ?>
                 </div>
+                <div class="cta-super">
+                    <p>Join IPLAY and become a Superuser too</p>
+                    <div class="buttons">
+                        <button type="button" name="button">Join IPLAY</button>
+                    </div>
+                </div>
+              </div>
 
 
                 <div class="athletes-images">
@@ -47,27 +57,22 @@ $superusers = collect(get_posts($args))->shuffle()->take(3);
                         <?php if (get_field('featured', $post_id) === 'Featured'): ?>
                         <?php $img = get_field('image', $post_id); ?>
                         <div class="skew-img">
-                            <img class="image-super" src="<?php echo $img['url'] ?>" alt="">
+                            <div class="image-super" style="background-image: url(<?php echo $img['url'] ?>);"></div>
                         </div>
                         <?php endif; ?>
                     <?php endforeach; ?>
                 </div>
             </div>
-            <div class="cta-super">
-                <p>Join IPLAY and become a SUPERUSER aswell!</p>
-                <div class="buttons">
-                    <button type="button" name="button">Join IPLAY</button>
-                </div>
-            </div>
+
 
 
         </div>
         <svg class="line-super" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         width="1282.88px" height="509.975px" viewBox="0 0 1282.88 509.975" style="enable-background:new 0 0 1282.88 509.975;"
         xml:space="preserve">
-        <line style="fill:none;stroke:#45E78E;stroke-width:3;stroke-miterlimit:10;" x1="22.499" y1="412.481" x2="418.479" y2="254.987"/>
-        <line style="fill:none;stroke:#45E78E;stroke-width:3;stroke-miterlimit:10;" x1="418.479" y1="254.987" x2="815.96" y2="461.979"/>
-        <line style="fill:none;stroke:#45E78E;stroke-width:3;stroke-miterlimit:10;" x1="815.96" y1="461.979" x2="1274.937" y2="206.992"/>
+        <line style="fill:none;stroke:#45E78E;stroke-width:2;stroke-miterlimit:10;" x1="22.499" y1="412.481" x2="418.479" y2="254.987"/>
+        <line style="fill:none;stroke:#45E78E;stroke-width:2;stroke-miterlimit:10;" x1="418.479" y1="254.987" x2="815.96" y2="461.979"/>
+        <line style="fill:none;stroke:#45E78E;stroke-width:2;stroke-miterlimit:10;" x1="815.96" y1="461.979" x2="1274.937" y2="206.992"/>
         </svg>
 
 
