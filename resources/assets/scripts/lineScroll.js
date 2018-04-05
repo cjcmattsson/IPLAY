843171c7b8043f0fcbe8dcda33fd4cbf4d125dd0
cjcmattsson <cjcmattsson@gmail.com>
2018-04-05 12:50:26 +0200

    Scrolling Line height fix

--- resources/assets/scripts/lineScroll.js
+++ resources/assets/scripts/lineScroll.js
@@ -9,9 +9,9 @@ content.addEventListener('scroll', function() {
   let scroll = content.scrollTop;
   if (previousPosition > currentPosition) {
     // console.log('scrolling up');
-    line.style.height = ((scroll+700)/1.65) + 'px';
+    line.style.height = ((scroll+700)/1.75) + 'px';
   } else {
-    line.style.height = ((scroll+700)/1.65) + 'px';
+    line.style.height = ((scroll+700)/1.75) + 'px';
     // console.log('scrolling down');
   }
   previousPosition = currentPosition;
