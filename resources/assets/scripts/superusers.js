c52685ecf1268f8bf6e62c1701c445b09a85578a
cjcmattsson <cjcmattsson@gmail.com>
2018-04-16 22:11:57 +0200

    Added final touches. Here goes nothing

--- resources/assets/scripts/superusers.js
+++ resources/assets/scripts/superusers.js
@@ -1,6 +1,6 @@
 const athleteImages = document.querySelector('.athletes-images');
 const athleteText = document.querySelector('.athletes-info-text');
-
+const content = document.querySelector('.parallax');
 
 window.setInterval( function() {
   const superuserImg = document.querySelector('.skew-img');
@@ -11,30 +11,12 @@ window.setInterval( function() {
 }, 2500)
 
 
+// Svg line superusers
+
 const lineSuperusers = document.querySelector('.line-super');
-
-const superusersLine = [lineSuperusers];
-
-var options = {
-  root: null,
-  rootMargin: '50px 0px',
-  threshold: 1
-}
-
-function onIntersection(entries) {
-  // Loop through the entries
-  entries.forEach(entry => {
-
-    // Are we in viewport?
-    if (entry.intersectionRatio > 0.5) {
-      console.log("hej");
-        entry.target.classList.add('line-animate-super');
-    }
-  });
-};
-
-var observer = new IntersectionObserver(onIntersection, options);
-
-superusersLine.forEach((line) => {
-  observer.observe(line);
-});
+content.addEventListener('scroll', () => {
+  console.log(content.scrollTop);
+  if (content.scrollTop >= 500) {
+    lineSuperusers.classList.add('line-animate-super');
+  }
+})
